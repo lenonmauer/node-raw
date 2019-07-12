3fe5683a8cc4b2229fc7b318f29e169ee380972a
Lenon Mauer <DATA52858@datainfo.inf.br>
2019-07-12 10:47:57 -0300

    add cache for static files

--- src/core/middlewares/static-files.js
+++ src/core/middlewares/static-files.js
@@ -1,20 +1,47 @@
 const path = require('path');
 const fs = require('fs');
-const { promisify } = require('util');
-const fileExistsAsync = promisify(fs.exists);
+const mime = require('mime-types');
+const recursive = require('recursive-readdir');
 
 class StaticFilesMiddleware {
-  serve = (dir) => async (req, res, next) => {
-    const url = req.url;
-    const file = path.join(dir, url);
+  serve = (dir) => {
+    const filesCached = [];
 
-    fs.stat(file, (err, stats) => {
-      if (err || !stats.isFile()) {
+    recursive(dir, function(err, files) {
+      files.forEach((file) => {
+        const filename = path.basename(file);
+
+        fs.stat(file, (err, stats) => {
+          if (err) return;
+
+          filesCached.push({
+            size: stats.size,
+            name: filename,
+            path: file,
+            type: mime.lookup(filename),
+          });
+        });
+      });
+    });
+
+    return (req, res, next) => {
+      if (req.method !== 'GET' && req.method !== 'HEAD') {
         return next();
       }
 
-      fs.createReadStream(file).pipe(res);
-    });
+      const url = req.url;
+      const filepath = path.join(dir, url);
+      const fileMatched = filesCached.find((fileCached) => fileCached.path === filepath);
+
+      if (!fileMatched) {
+        return next();
+      }
+
+      res.setHeader('Content-type', mime.contentType(fileMatched.type));
+      res.setHeader('Content-length', fileMatched.size);
+
+      fs.createReadStream(filepath).pipe(res);
+    };
   };
 }
 
